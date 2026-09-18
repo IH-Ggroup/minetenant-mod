e9aaa448de6a1360fb7ce7426968f9300d15c9f9
takarea <157473679+takarea@users.noreply.github.com>
2026-09-18 12:18:46 +0900

    add:MinetenantClient

---
+++ MinetenantClient/src/client/java/com/minetenant/minetenantclient/client/MinetenantclientClient.java
@@ -0,0 +1,10 @@
+package com.minetenant.minetenantclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinetenantclientClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ MinetenantClient/src/client/java/com/minetenant/minetenantclient/client/MinetenantclientDataGenerator.java
@@ -0,0 +1,12 @@
+package com.minetenant.minetenantclient.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MinetenantclientDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
